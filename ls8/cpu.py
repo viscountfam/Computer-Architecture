025a66c413f925fa9f227712603074475db80d22
Michael Famurewa <myfamurewa@gmail.com>
2020-08-19 17:31:44 -0400

    day 2 completed

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,7 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.register = [0] * 8
+        self.reg = [0] * 8
         self.ram =  [0] * 256
         self.pc = 0
 
@@ -17,41 +17,48 @@ class CPU:
     def ram_write(self, address, value):
         self.ram[address] = value
 
-    def load(self):
+    def load(self, file_name):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
-        if len(sys.argv) < 2:
-          program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-            ] 
-          for instruction in program:
-                self.ram[address] = instruction
+        # # For now, we've just hardcoded a program:
+        # if len(sys.argv) < 2:
+        #   program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        #     ] 
+        #   for instruction in program:
+        #         self.ram[address] = instruction
+        #         address += 1
+        # elif len(sys.argv) == 2:
+        #     filename = sys.argv[1]
+        #     with open(filename) as f:
+        #         address = 0
+        #         for line in f:
+        #             if lin[0] != "#" or line[0] != "":
+        #                 line = line.split("#")
+        #                 try:
+        #                     v = int(line[0], 2)
+        #                 except ValueError:
+        #                     continue
+        #                 self.ram[address] = v
+        #                 address += 1
+        # else:
+        #     print("Only one ls8 program can be loaded")
+        #     sys.exit("too many files present")
+        with open(f"examples/{file_name}") as f:
+            for line in f:
+                line = line.split("#")
+                line = line[0].strip()
+                if line:
+                    self.ram[address] = int(line, 2)
                 address += 1
-        elif len(sys.argv) == 2:
-            filename = sys.argv[1]
-            with open(filename) as f:
-                address = 0
-                for line in f:
-                    if lin[0] != "#" or line[0] != "":
-                        line = line.split("#")
-                        try:
-                            v = int(line[0], 2)
-                        except ValueError:
-                            continue
-                        self.ram[address] = v
-                        address += 1
-        else:
-            print("Only one ls8 program can be loaded")
-            sys.exit("too many files present")
 
 
 
@@ -65,6 +72,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -93,24 +102,25 @@ class CPU:
         """Run the CPU."""
         running = True
         # ir = self.ram_read(self.pc)
-        # print("IR", bin(ir))
         while running:
+            # print("it's running again")
             ir = self.ram_read(self.pc)
+            # print("IR", ir)
             if ir == 0b10000010:
-                # print("self.ram", self.ram)
-                # register_address = self.ram_read(self.pc + 1)
-                # print("register address", self.ram_read(self.pc + 1))
-                # print("self.register", self.register)
-                # print("self.pc", self.pc)
-                self.register[self.ram_read(self.pc + 1)] = self.ram_read(self.pc + 2)
+               
+                self.reg[self.ram_read(self.pc + 1)] = self.ram_read(self.pc + 2)
                 self.pc += 3
             elif ir == 0b01000111: 
                 register_address = self.ram_read(self.pc + 1)
-                print(self.register[register_address])
+                print(self.reg[register_address])
                 self.pc += 2
             elif ir == 0b10100010:
-                self.register[self.ram_read(self.pc + 1)] = self.ram_read(self.pc + 2) * self.ram_read(self.pc + 3)
-                self.pc += 4
+                # self.reg[self.ram_read(self.pc + 1)] = self.ram_read(self.pc + 2) * self.ram_read(self.pc + 3)
+                # self.pc += 3
+                reg_A = self.ram_read(self.pc + 1)
+                reg_B = self.ram_read(self.pc + 2)
+                self.alu("MUL", reg_A, reg_B)
+                self.pc += 3
             elif ir == 0b00000001:
                 running = False
                 
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 
 """Main."""
-
+from os import listdir
 import sys
 from cpu import *
 
+
 cpu = CPU()
 
-cpu.load()
+# cpu.load()
+# cpu.run()
+
+files = {file for file in listdir("examples")}
+
+if len(sys.argv) > 1:
+    file_name = sys.argv[1]
+    if file_name in files:
+        cpu.load(file_name)
+    else:
+        print("An invalid file was selected")
+        sys.exit()
+else:
+    cpu.load("print8.ls8")
 cpu.run()
